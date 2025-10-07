788849274e161c0d26ceff855442fd27443f547b
dewi anggraini <240103093@mhs.udb.ac.id>
2025-10-07 15:38:51 +0700

    Add files via upload

---
+++ p.py
@@ -0,0 +1,8 @@
+lirik =  [
+    "saat engkau tertidur aku pergi menghibur",
+    "beda kota pisah raga bukan masalahku",
+    "lihat wajahmu dilayar tetap tersenyum",
+]
+
+for baris in lirik:
+    print(baris)
